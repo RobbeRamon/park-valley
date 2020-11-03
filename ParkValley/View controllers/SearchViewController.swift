94864494e37fe814a0dfcbae4c9c60c52c9aa85c
RobbeRamon <robbe.ramon@student.hogent.be>
2020-11-04 00:03:30 +0100

    Add placeholder when there are no recently visited garages on the search page

--- ParkValley/View controllers/SearchViewController.swift
+++ ParkValley/View controllers/SearchViewController.swift
@@ -20,11 +20,20 @@ class SearchViewController: UIViewController  {
         overlay.backgroundColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.5)
         ivFindNearby.addSubview(overlay)
         
-        recentlyVisited.append("New York")
-        recentlyVisited.append("Japan")
+//        recentlyVisited.append("New York")
+//        recentlyVisited.append("Japan")
         
         tvRescentlyVisited.delegate = self
         tvRescentlyVisited.dataSource = self
+        
+        if recentlyVisited.isEmpty {
+            let label : UILabel = UILabel()
+            label.text = "Nothing found"
+            label.textAlignment = .center
+            label.textColor = .gray
+            
+            tvRescentlyVisited.backgroundView = label
+        }
     }
     
     /*
@@ -39,6 +48,8 @@ class SearchViewController: UIViewController  {
 
 }
 
+// MARK: - TableView data source and delegate
+
 extension SearchViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("You tapped me!")
@@ -46,15 +57,49 @@ extension SearchViewController: UITableViewDelegate {
 }
 
 extension SearchViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recentlyVisited.count
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return recentlyVisited.count
+        } else {
+            return 0
+        }
+
+    }
+    
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        if section == 0 {
+//            return "Recently visited"
+//        } else {
+//            return ""
+//        }
+//    }
+//
+//    /**
+//     SOURCE:  https://stackoverflow.com/questions/19802336/changing-font-size-for-uitableview-section-headers
+//     */
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let myLabel = UILabel()
+//        let font : UIFontDescriptor = UIFontDescriptor(name: "PingFang SC", size: 20)
+//
+//        myLabel.frame = CGRect(x: 20, y:8, width: 320, height: 20)
+//        myLabel.font = UIFont.init(descriptor: font, size: 20)
+//        myLabel.textAlignment = .center
+//        myLabel.text = self.tableView(tableView, titleForHeaderInSection: section)
+//
+//        let headerView = UIView()
+//        headerView.addSubview(myLabel)
+//
+//        return headerView
+//    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "garage", for: indexPath)
-        let garage = recentlyVisited[indexPath.row]
-        
-        cell.textLabel?.text = garage
+        cell.textLabel?.text = recentlyVisited[indexPath.row]
         
         return cell
     
