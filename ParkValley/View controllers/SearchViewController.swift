283ad56a0acff2482ba3b387ab8e91388abd4cfa
RobbeRamon <robbe.ramon@student.hogent.be>
2020-11-03 23:08:24 +0100

    Add search page with tableview to show the rescently visited garages (dummy data)

--- ParkValley/View controllers/SearchViewController.swift
+++ ParkValley/View controllers/SearchViewController.swift
@@ -7,15 +7,26 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UIViewController  {
+    
+    @IBOutlet var ivFindNearby: UIImageView!
+    @IBOutlet var tvRescentlyVisited: UITableView!
+    private var recentlyVisited : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let overlay: UIView = UIView(frame: CGRect(x: 0, y: 0, width: ivFindNearby.frame.size.width, height: ivFindNearby.frame.size.height))
+        overlay.backgroundColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.5)
+        ivFindNearby.addSubview(overlay)
+        
+        recentlyVisited.append("New York")
+        recentlyVisited.append("Japan")
+        
+        tvRescentlyVisited.delegate = self
+        tvRescentlyVisited.dataSource = self
     }
     
-
     /*
     // MARK: - Navigation
 
@@ -26,4 +37,27 @@ class SearchViewController: UIViewController {
     }
     */
 
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped me!")
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recentlyVisited.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "garage", for: indexPath)
+        let garage = recentlyVisited[indexPath.row]
+        
+        cell.textLabel?.text = garage
+        
+        return cell
+    
+    }
+    
 }
