561fe67bbd0ae78a6a6b3e332c76dc3a088c18b9
RobbeRamon <robbe.ramon@student.hogent.be>
2020-11-08 09:53:18 +0100

    Add Garage and User model

---
+++ ParkValley/Model/Garage.swift
@@ -0,0 +1,17 @@
+//
+//  Garage.swift
+//  ParkValley
+//
+//  Created by Robbe on 08/11/2020.
+//
+
+import Foundation
+
+struct Garage {
+    var id : String
+    var name : String
+    var city : String
+    var latitude : Double
+    var longitude : Double
+    var user : User
+}
---
+++ ParkValley/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  ParkValley
+//
+//  Created by Robbe on 08/11/2020.
+//
+
+import Foundation
+
+struct User {
+    var id : String
+    var username : String
+}
