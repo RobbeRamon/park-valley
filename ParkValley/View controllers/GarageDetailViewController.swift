0b459278148fcbbc609c9dfa1de015d195386961
Robbe Ramon <robbe.ramon@student.hogent.be>
2020-11-23 18:33:18 +0100

    Add card view to detail garage page with annotation

--- ParkValley/View controllers/GarageDetailViewController.swift
+++ ParkValley/View controllers/GarageDetailViewController.swift
@@ -6,11 +6,12 @@
 //
 
 import UIKit
+import MapKit
 
 class GarageDetailViewController: UIViewController {
     
-    @IBOutlet var lblName: UILabel!
-    @IBOutlet var ivImage: UIImageView!
+    @IBOutlet var niName: UINavigationItem!
+    @IBOutlet var mvMap: MKMapView!
     
     var garage: Garage?
 
@@ -32,9 +33,47 @@ class GarageDetailViewController: UIViewController {
     // MARK: - Helper functions
     func updateUI() {
         if let garage = self.garage {
-            lblName.text = garage.name
+            niName.title = garage.name
+            
+            let initialLocation = CLLocation(latitude: garage.latitude!, longitude: garage.longitude!)
+            mvMap.centerToLocation(initialLocation)
+            
+//            let oahuCenter = CLLocation(latitude: garage.latitude!, longitude: garage.longitude!)
+//            let region = MKCoordinateRegion(
+//              center: oahuCenter.coordinate,
+//              latitudinalMeters: 50000,
+//              longitudinalMeters: 60000)
+//            mvMap.setCameraBoundary(
+//              MKMapView.CameraBoundary(coordinateRegion: region),
+//              animated: true)
+//            
+//            let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+//            mvMap.setCameraZoomRange(zoomRange, animated: true)
+            
+            addAnnotationToMap(name: garage.name!, latitude: garage.latitude!, longitude: garage.longitude!)
         }
     }
     
+    // MARK: - Helper methods
+    func addAnnotationToMap(name: String, latitude: Double, longitude: Double) {
+        let garage = MKPointAnnotation()
+        garage.title = name
+        garage.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        mvMap.addAnnotation(garage)
+    }
 
+}
+
+// SOURCE: https://www.raywenderlich.com/7738344-mapkit-tutorial-getting-started
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 1000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
 }
