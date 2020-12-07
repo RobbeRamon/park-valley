65f5b6910d3e239a7237ce143d559d7c891f0125
Robbe Ramon <robbe.ramon@student.hogent.be>
2020-12-07 13:28:56 +0100

    Add possibility to get the locatoin of the user

--- ParkValley/View controllers/GarageShowMapViewController.swift
+++ ParkValley/View controllers/GarageShowMapViewController.swift
@@ -7,14 +7,30 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class GarageShowMapViewController: UIViewController {
     
     @IBOutlet var mvMap: MKMapView!
     var garage: Garage?
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
         updateUI()
     }
     
@@ -53,5 +69,12 @@ private extension MKMapView {
       longitudinalMeters: regionRadius)
     setRegion(coordinateRegion, animated: true)
   }
+}
+
+extension GarageShowMapViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
 }
 
